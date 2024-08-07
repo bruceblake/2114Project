6d222df1bd6755538788284da3a4187000cbc39f
bruceblake <bruceiiiblake@gmail.com>
2024-08-06 22:52:14 -0700

    First commit for 2114 project

---
+++ SAList/SAList.java
@@ -0,0 +1,59 @@
+public class SAList {
+  private ListNode front; // first node of this list (null if empty)
+  private int listSize; // the number of elements in this list
+  private int remIndex; // the index of the remembered node
+  private ListNode remNode; // a reference to the node accessed by most recent call to get
+
+  /** Constructs an empty SAList. */
+  public SAList() {
+    front = null;
+    remIndex = -1;
+    remNode = null;
+    listSize = 0;
+  }
+
+  public int size() {
+    return listSize;
+  }
+
+  public void addFirst(Object obj) {
+    ListNode node = new ListNode(obj, front);
+    front = node;
+    listSize++;
+  }
+
+  public Object get(int n) {
+    // 1 -> 2 -> 3 -> 4 -> 5
+    // f
+    ListNode current;
+    int currentIndex;
+
+    if (remIndex <= n && remNode != null) {
+      current = remNode;
+      currentIndex = remIndex;
+    } else {
+      current = front;
+      currentIndex = 0;
+    }
+
+    while (currentIndex < n) {
+      current = current.getNext();
+      currentIndex++;
+    }
+
+    remNode = current;
+    remIndex = currentIndex;
+
+    return current.getValue();
+
+  }
+
+  public int getMostRecentIndex() {
+
+  }
+
+  public Object getMostRecentValue() {
+
+  }
+
+}
